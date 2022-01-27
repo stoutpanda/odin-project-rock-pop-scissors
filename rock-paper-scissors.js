42dd7f415b78ffa1ccbe160410f6cd428bb3b183
stoutpanda <stoutpanda@protonmail.com>
2022-01-26 22:25:39 -0600

    Created playRound functionality -- bug found.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,6 +2,10 @@
 
 const selection = ["Rock", "Paper", "Scissors"];
 const invalidInput = "Not A Valid Selection";
+const modNum = selection.length; 
+
+
+
 
 //Computer Selection
 function computerPlay() {
@@ -27,5 +31,37 @@ function playerSelection(playerInput) {
         console.log(invalidInput);
     }
 
+}
+
+function playRound(playerInput, computerInput) {
+
+   //check players selection, use position within array as value, subtract and modulous. 0 should be tie, odds loss and evens wins. 
+
+
+   // Invalid test case: 
+   //You Loose! You chose Paper & the Computer chose: Rock
+   const playersChoice = playerSelection(playerInput);
+   const playersValue = selection.indexOf(playersChoice);
+   const computersValue = selection.indexOf(computerInput);
+   let response = "You chose " + playersChoice + " & the Computer chose: " + computerInput;
+   
+   const result = ((playersValue - computersValue) % modNum);
+   const resultIsEven = ((result % 2) === 0) ? true : false; 
+
+   console.log(result)
+
+   if(result === 0 ) {
+        response = "You Tied! " + response;
+   
+   }
+   else if (resultIsEven) {
+       response = "You Win! " + response;
+   }
+   else {
+       response = "You Loose! " + response; 
+   }
+
+   return response;
 
 }
+
