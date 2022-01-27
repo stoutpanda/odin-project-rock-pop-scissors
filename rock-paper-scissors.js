38109161e45e674a1299539d43393cbf6491619e
stoutpanda <stoutpanda@protonmail.com>
2022-01-26 20:44:26 -0600

    Created playerSelection function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,31 @@
 // Rock Paper Scissors
 
-
+const selection = ["Rock", "Paper", "Scissors"];
+const invalidInput = "Not A Valid Selection";
 
 //Computer Selection
 function computerPlay() {
-    const selection = ["Rock", "Paper", "Scissors"];
+
     const random = Math.floor(Math.random() * selection.length);
     return selection[random];
 }
 
+function playerSelection(playerInput) {
+
+    //take players input, convert to string, downcase string, take char at first position and upcase and rejoin, verify that in selection array
+    const stringInput = (typeof (playerInput) === 'string') ? playerInput : (playerInput.toString());
+    const lowercaseInput = stringInput.toLowerCase();
+    const capitalFirst = lowercaseInput.charAt(0).toUpperCase();
+    const normalizedInput = capitalFirst + lowercaseInput.slice(1);
+
+    const verifiedInput = selection.includes(normalizedInput) ? normalizedInput : invalidInput;
+
+    if (verifiedInput !== invalidInput) {
+        return verifiedInput;
+    }
+    else {
+        console.log(invalidInput);
+    }
+
+
+}
